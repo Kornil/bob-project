05dfdc31a1ff2300df3cf863e22452bd92bcea4c
Francesco Agnoletto <francesco.agnoletto92@gmail.com>
2018-11-21 20:35:48 +0100

    Fix test config

--- jest.config.js
+++ jest.config.js
@@ -1,20 +1,20 @@
 module.exports = {
-  moduleFileExtensions: ["js", "jsx", "json", "node"],
-  moduleDirectories: ["node_modules", "."],
-  testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.(js|jsx)$",
+  moduleFileExtensions: ['js', 'jsx', 'json', 'node'],
+  moduleDirectories: ['node_modules', '.'],
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(js|jsx)$',
   transform: {
-    "^.+\\.(tsx?|jsx?)$": "babel-jest"
+    '^.+\\.(tsx?|jsx?)$': 'babel-jest',
   },
-  snapshotSerializers: ["enzyme-to-json/serializer"],
-  setupTestFrameworkScriptFile: "<rootDir>/src/client/tests/setup.js",
+  snapshotSerializers: ['enzyme-to-json/serializer'],
+  setupTestFrameworkScriptFile: '<rootDir>/src/tests/setup.js',
   collectCoverage: true,
-  coverageDirectory: "./coverage/",
-  collectCoverageFrom: ["src/**/*.{js,jsx}"],
+  coverageDirectory: './coverage/',
+  collectCoverageFrom: ['src/**/*.{js,jsx}'],
   testURL: 'http://localhost',
   coveragePathIgnorePatterns: [
-    "/node_modules/",
-    "/tests/",
-    "/src/client/index.jsx",
-    "/src/server/index.js"
-  ]
+    '/node_modules/',
+    '/tests/',
+    '/src/client/index.jsx',
+    '/src/server/index.js',
+  ],
 };
