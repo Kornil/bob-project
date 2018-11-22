63715ef227181d4ae3fe61cafe977e75f1a83493
Francesco Agnoletto <francesco.agnoletto92@gmail.com>
2018-11-22 10:13:56 +0100

    Update Home tests

--- src/client/containers/Home/Home.test.jsx
+++ src/client/containers/Home/Home.test.jsx
@@ -1,10 +1,25 @@
 import React from 'react';
 import { render, unmountComponentAtNode } from 'react-dom';
+import fetchMock from 'fetch-mock';
+import { shallow } from 'enzyme';
 
 import Home from '.';
 
-it('renders correctly', () => {
-  const div = document.createElement('div');
-  render(<Home />, div);
-  unmountComponentAtNode(div);
+fetchMock.get('*', JSON.stringify({ users: [{ _id: 123, user: { name: 'asd', bags: 4 } }] }));
+
+describe('<Home />', () => {
+  it('renders correctly', () => {
+    const div = document.createElement('div');
+    render(<Home />, div);
+    unmountComponentAtNode(div);
+  });
+  it('renders cards correctly', async () => {
+    const wrapper = shallow(<Home />);
+
+    const instance = wrapper.instance();
+
+    await instance.fetchData();
+
+    expect(wrapper.find('Card')).toHaveLength(1);
+  });
 });
