f6d67c0d507d77b2cb7d389c390ae1b09c4337f9
Francesco Agnoletto <francesco.agnoletto92@gmail.com>
2018-11-22 16:19:31 +0100

    Test NewUserForm

--- src/client/containers/Home/components/NewUserForm.test.jsx
+++ src/client/containers/Home/components/NewUserForm.test.jsx
@@ -1,10 +1,25 @@
 import React from 'react';
 import { render, unmountComponentAtNode } from 'react-dom';
+import { mount } from 'enzyme';
+import fetchMock from 'fetch-mock';
 
 import NewUserForm from './NewUserForm';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  render(<NewUserForm fetchData={() => null} />, div);
-  unmountComponentAtNode(div);
+fetchMock.post('/new', 200);
+
+describe('<NewUserForm />', () => {
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    render(<NewUserForm fetchData={() => null} />, div);
+    unmountComponentAtNode(div);
+  });
+
+  it('fetches data again after successful post', () => {
+    const fakeFetch = jest.fn();
+    const wrapper = mount(<NewUserForm fetchData={fakeFetch} />);
+
+    wrapper.find('form').simulate('submit');
+
+    expect(fakeFetch.mock.calls.length).toEqual(1);
+  });
 });
